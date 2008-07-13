e2e373078a85fdead25546c583c669bc2231e5f6
Evan Broder <broder@mit.edu>
2008-07-13 18:43:13 +0000

    Add a validator to the UserSelect dropdown
    Converts back and forth between a SQLAlchemy object and the numeric ID

--- bluechips/widgets/__init__.py
+++ bluechips/widgets/__init__.py
@@ -1,5 +1,7 @@
 from tw import forms
 
+from tw.forms import validators
+
 from bluechips import model
 from bluechips.model import meta
 
@@ -7,8 +9,17 @@ class UserSelect(forms.SingleSelectField):
     @staticmethod
     def getUserList():
         for u in meta.Session.query(model.User):
-            yield (u.username, u.name)
+            yield (u.id, u.name)
     
     options = getUserList
+    validator = validators.Wrapper(
+        to_python=meta.Session.query(model.User).get,
+        from_python=(lambda x: x.id))
 
-__all__ = ['UserSelect']
+class AmountField(forms.TextField):
+    size = 8
+    validator = validators.All(
+        validators.Number(),
+        validators.Regex(r'^[0-9]*(\.[0-9]{2})?$', not_empty=True))
+
+__all__ = ['UserSelect', 'AmountField']
