ef8d2fbb554f810e77966c41cce07d948a683cf0
Evan Broder <broder@mit.edu>
2008-07-14 06:13:09 +0000

    Add new action to spend controller for testing validation

--- bluechips/controllers/spend.py
+++ bluechips/controllers/spend.py
@@ -12,3 +12,7 @@ log = logging.getLogger(__name__)
 class SpendController(BaseController):
     def index(self):
         return render('/spend/index.mako')
+    
+    @validate(form=spend.new_spend_form, error_handler='index')
+    def new(self):
+        return str(self.form_result)
