6fd3a2f6b196a03a9dd90133aa0211a321977c59
Evan Broder <broder@mit.edu>
2008-07-20 14:53:07 +0000

    Add another test for testing uneven splits

--- bluechips/tests/splits/test_fixed.py
+++ bluechips/tests/splits/test_fixed.py
@@ -7,6 +7,9 @@ from decimal import Decimal
 
 class TestSplitFixed(TestCase):
     def test_simpleSplit(self):
+        """
+        Test simply splitting a $100 expenditure amongst 4 people
+        """
         createUsers(4)
         
         e = model.Expenditure()
@@ -19,6 +22,63 @@ class TestSplitFixed(TestCase):
         for s in meta.Session.query(model.Split).\
                 filter(model.Split.expenditure==e):
             self.assertEqual(s.share, Currency("25.00"))
+        
+        deleteExpenditures()
+        deleteUsers()
+    
+    def test_uneven(self):
+        """
+        Test that expenditures can be split non-evenly
+        """
+        createUsers(2)
+        
+        users = meta.Session.query(model.User).all()
+        
+        e = model.Expenditure()
+        e.spender = users[0]
+        e.amount = Currency("100.00")
+        meta.Session.save(e)
+        
+        split_dict = {users[0]: Decimal("20"),
+                      users[1]: Decimal("80")}
+        
+        amount_dict = {users[0]: Currency("20"),
+                       users[1]: Currency("80")}
+        
+        e.split(split_dict)
+        meta.Session.commit()
+        
+        for s in meta.Session.query(model.Split):
+            self.assertEqual(s.share, amount_dict[s.user])
+        
+        deleteExpenditures()
+        deleteUsers()
+    
+    def test_unevenBadTotal(self):
+        """
+        Test that transactions get split up properly when the uneven
+        split shares don't add to 100%
+        """
+        createUsers(2)
+        
+        users = meta.Session.query(model.User).all()
+        
+        e = model.Expenditure()
+        e.spender = users[0]
+        e.amount = Currency("100.00")
+        meta.Session.save(e)
+        
+        split_dict = {users[0]: Decimal(10),
+                      users[1]: Decimal(15)}
+        
+        amount_dict = {users[0]: Currency("40"),
+                       users[1]: Currency("60")}
+        
+        e.split(split_dict)
+        meta.Session.commit()
+        
+        for s in meta.Session.query(model.Split):
+            self.assertEqual(s.share, amount_dict[s.user])
         
         deleteExpenditures()
         deleteUsers()
