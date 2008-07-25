a1383b8d879cdb414f922af4facc51892fa304d3
Evan Broder <broder@mit.edu>
2008-07-25 02:07:51 +0000

    Add an __init__ method for Transfer objects

--- bluechips/model/transfer.py
+++ bluechips/model/transfer.py
@@ -1,4 +1,11 @@
+from types import Currency
+
 class Transfer(object):
+    def __init__(self, debtor=None, creditor=None, amount=Currency(0)):
+        self.debtor = debtor
+        self.creditor = creditor
+        self.amount = amount
+    
     def __repr__(self):
         return '<Transfer: from %s to %s for %s>' % (self.debtor,
                                                      self.creditor,
