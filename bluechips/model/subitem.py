1ff296f23a4f7f54b02ec4d42a5ec19fbf1d72bf
Evan Broder <broder@mit.edu>
2008-07-25 02:07:45 +0000

    Add an __init__ method for Subitem objects

--- bluechips/model/subitem.py
+++ bluechips/model/subitem.py
@@ -1,4 +1,11 @@
+from types import Currency
+
 class Subitem(object):
+    def __init__(self, expenditure=None, user=None, amount=Currency(0)):
+        self.expenditure = expenditure
+        self.user = user
+        self.share = share
+        
     def __repr__(self):
         return '<Subitem: expense: %s user: %s cost: %s>' % (self.expense,
                                                              self.user,
