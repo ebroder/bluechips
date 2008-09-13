324dd95a63d2f1fccadf702d279871e53a6e65fc
Evan Broder <broder@mit.edu>
2008-09-13 03:24:13 +0000

    Update status page to actually get 10 recent transations

--- bluechips/controllers/status.py
+++ bluechips/controllers/status.py
@@ -42,8 +42,8 @@ class StatusController(BaseController):
         c.expenditures = meta.Session.query(model.Expenditure).join('credits').\
             reset_joinpoint().join('debits').filter(sqlalchemy.or_(
                 model.Credit.account==account,
-                model.Debit.account==account)).limit(10)
-            
+                model.Debit.account==account)).\
+                group_by(model.Expenditure.id).limit(10)
         
         return render('/status/index.mako')
     
