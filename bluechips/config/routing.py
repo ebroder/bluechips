a2f488577b06f1cb644d4417443d63559523109c
Evan Broder <broder@mit.edu>
2008-09-13 03:24:09 +0000

    Make template controller last line of defense against 404s

--- bluechips/config/routing.py
+++ bluechips/config/routing.py
@@ -22,5 +22,6 @@ def make_map():
     map.connect('/:controller')
     map.connect('/:controller/:action')
     map.connect('/:controller/:action/:id')
+    map.connect('/*url', controller='template', action='index')
 
     return map
