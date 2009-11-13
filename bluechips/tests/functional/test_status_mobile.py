3639b85127f2b7e87f9818f8c4d3340cb8f0639c
Scott Torborg <scott@crookedmedia.com>
2009-11-12 21:02:47 -0800

    added some basic tests for mobile interface, including switching back and forth with normal interface

---
+++ bluechips/tests/functional/test_status_mobile.py
@@ -0,0 +1,19 @@
+from bluechips.tests import *
+
+class TestMobileController(TestController):
+    def setUp(self):
+        self.ua = ('Mozilla/5.0 (iPhone; U; CPU like Mac OS X; en) '
+                   'AppleWebKit/420+ (KHTML, like Gecko) Version/3.0 '
+                   'Mobile/1A543a Safari/419.3')
+        self.app.extra_environ['HTTP_USER_AGENT'] = self.ua
+
+    def test_switch_interfaces(self):
+        response = self.app.get('/')
+        response.mustcontain('apple-touch-icon')
+        response.mustcontain('Use non mobile interface')
+        response = response.click('Use non mobile interface')
+        response.mustcontain('Use mobile interface')
+        response = response.click('Use mobile interface')
+
+    def test_view_nonmobile(self):
+        self.app.get(url_for(controller='history'))
