cd859ff2b7d001c7b3d53eb1be0d3a2c5164d4a7
Evan Broder <broder@mit.edu>
2010-02-25 00:35:58 -0500

    Push a new alpha.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except ImportError:
 
 setup(
     name='BlueChips',
-    version='1.0a2',
+    version='1.0a3',
     description='BlueChips - finances for people with shared expenses',
     long_description=open('README.rst').read(),
     author='Residents of Blue Sun Corporate Headquarters',
