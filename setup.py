b829c4579fe6d904d01d0621aaa6b78eced08747
Evan Broder <broder@mit.edu>
2010-11-20 22:49:49 -0800

    Bump version number

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except ImportError:
 
 setup(
     name='BlueChips',
-    version='1.0.0',
+    version='1.0.1',
     description='BlueChips - finances for people with shared expenses',
     long_description=open('README.rst').read(),
     author='Residents of Blue Sun Corporate Headquarters',
